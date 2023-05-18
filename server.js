0c623de23709db00b2c9c5bc23afa36f2519269c
sumurillo <sumire.ohana@gmail.com>
2023-05-18 10:10:01 -0500

    install cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const cors = require('cors');
+const port = process.env.PORT || 3001
 
 const dogsRouter = require('./routes/dogs')
 
@@ -12,6 +13,7 @@ require('./config/database')
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // ROUTES
 app.use('/dogs', dogsRouter);
